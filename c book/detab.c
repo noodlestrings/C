ab723c26320584fac89011e1718c43b6cbd2b48d
Macbook air <sammystradling2006@gmail.com>
2022-10-16 20:13:14 +0100

    input and tab detection working. no replacement yet

--- c book/detab.c
+++ c book/detab.c
@@ -1,48 +1,25 @@
 #include <stdio.h>
-#include <string.h>
+const int sentenceLen = 120;
 
-// replaces tab character in array with 4 spaces
-
-//tab is 4 spaces
 int main(){
-	char inp[100];
-	int tabLocations[25];
-	memset(tabLocations, 0, sizeof(tabLocations)); // sets all values to 0
-	int tabLocFillAmount = 0;
 
-	printf("Please enter a sentence with tabs in it: ");
-	scanf("%[^\n]%*c", inp);
+	char sentence[sentenceLen];
 
-	for(int i=0;i<100;i++){
-		if (inp[i] == '\0'){
-			break; //if EOF
-		}
-		if (inp[i] == '\t'){
-			tabLocations[tabLocFillAmount] = i;
-			tabLocFillAmount++; // this essentially appends to end of array
-
-			for(int j = 100;j>i - 1;j++){
-				if //trying to move latter charcaters by 4 to insert 4 spaces
-					//look up inserting values in an array
-			}
-		}
+	printf("Enter a sentence with tabs: ");
+	scanf("%[^\n]%*c", sentence);
 
 
-		
-	}
-	for (int i=0;i<25;i++){
-		if(tabLocations[i] == '\0'){
+	for (int i = 0; i<sentenceLen; i++){
+		if(sentence[i] == '\0'){
 			break;
 		}
-		inp[i] = "    ";
-	}
-
-	for(int i=0;i<100;i++){
-		if (inp[i] == '\0'){
-			break;
+		if(sentence[i] == '\t'){
+			printf("tab");
 		}
-		printf("%c",inp[i] );
+		printf("%c", sentence[i]);
 	}
 
+
+	printf("\n");
 	return 0;
 }
