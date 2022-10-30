13cd96e3eb01dbbf85c34ed8a6dffa1e06a68008
Macbook air <sammystradling2006@gmail.com>
2022-10-30 17:15:17 +0000

    working detab

--- c book/detab.c
+++ c book/detab.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+
+// replaces tabs with "  tab  "
+
 const int sentenceLen = 120;
+
+void replace_tab(char *arr, int tabLocation){
+	for(int i=sentenceLen-1;i>tabLocation;i--){
+		if(arr[i]!='\0'){
+			arr[i+6] = arr[i];
+		}
+	}
+	arr[tabLocation] = ' ';
+	arr[tabLocation+1] = ' ';
+	arr[tabLocation+2] = 'T';
+	arr[tabLocation+3] = 'A';
+	arr[tabLocation+4] = 'B';
+	arr[tabLocation+5] = ' ';
+	arr[tabLocation+6] = ' ';
+
+}
 
 int main(){
 
 	char sentence[sentenceLen];
+	memset(sentence, 0, sizeof(sentence));
 
 	printf("Enter a sentence with tabs: ");
 	scanf("%[^\n]%*c", sentence);
@@ -14,7 +35,10 @@ int main(){
 			break;
 		}
 		if(sentence[i] == '\t'){
-			printf("tab");
+			sentence[i] = ' ';
+
+			replace_tab(sentence, i);
+
 		}
 		printf("%c", sentence[i]);
 	}
